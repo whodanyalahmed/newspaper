d1c671f05b6bee4523cd72f28005252a0a33a6a7
whodanyalahmed <Daniahmedkhatri@gmail.com>
2019-05-16 20:15:56 +0500

    setup added

---
+++ setup.py
@@ -0,0 +1,7 @@
+setup(
+    name='newspaper_project',
+    version='0.1.0',
+    install_requires=[],  # Don't put anything here, just use requirements.txt
+    packages=['newspaper_project'],
+    package_dir={'newspaper_project': 'src/newspaper_project'},
+)
